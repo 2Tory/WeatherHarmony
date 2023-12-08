2112001114dd15d1628e9b6e8123c0729bf9a7c0
2Tory <dldudgus0604@gmail.com>
2023-12-08 20:47:22 +0900

    Initial commit

---
+++ controllers/controller.js
@@ -0,0 +1,59 @@
+const axios = require('axios');
+
+let lat;
+let lon;
+const owmApiKey = '8df00d070b08179e231e7ba594de2e3c';
+const spotifyApiKey = '';
+let city = 'Busan';  // 원하는 도시명으로 변경 가능
+
+const searchCityWeatherApiUrl = () => `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${owmApiKey}`;
+const currentPositionWeatherApiUrl = () => `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${owmApiKey}`;
+
+const controller ={
+    index: (req,res) => {
+        axios.get(searchCityWeatherApiUrl())
+            .then(response => {
+                const weatherData = response.data;
+                console.log('Weather Data:', weatherData);
+                // 여기에서 날씨 정보를 가지고 원하는 작업을 수행하세요
+                res.render('index',{weatherData:weatherData});
+            })
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+            });
+
+
+    },
+
+    getCurrentPositionWeather: (req, res) => {
+        lon = req.body.lon;
+        lat = req.body.lat;
+
+        axios.get(currentPositionWeatherApiUrl())
+            .then(response => {
+                const weatherData = response.data;
+                console.log('Weather Data:', weatherData);
+                res.json(weatherData);
+                // 여기에서 날씨 정보를 가지고 원하는 작업을 수행하세요
+            })
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+            });
+    },
+    getSearchCityWeather: (req, res) => {
+        city = req.body.city;
+
+        axios.get(searchCityWeatherApiUrl())
+            .then(response => {
+                const weatherData = response.data;
+                console.log('Weather Data:', weatherData);
+                // 여기에서 날씨 정보를 가지고 원하는 작업을 수행하세요
+                res.json(weatherData);
+            })
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+            });
+    }
+}
+
+module.exports = {controller};
---
+++ routes/route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { controller } = require('../controllers/controller');
+
+router.get('/', controller.index);
+router.post('/currentPosition', controller.getCurrentPositionWeather);
+module.exports = router;
